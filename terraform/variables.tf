8fccb058cdcb1c0af94192cf26c3acf8445db94a
Raul Melo <50232299+Raulcarvalhomelo@users.noreply.github.com>
2026-08-06 01:56:25 +0000

    variable

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "tenancy_ocid"{
+    type = string
+}
+variable "user_ocid"{
+    type = string
+}
+variable "fingerprint"{
+    type = string
+}
+variable "region"{
+    type = string
+}
+variable "private_key"{
+    type = string
+    sensitive = true
+}
