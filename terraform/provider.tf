621f565152a1de2da26ee5c0fd2e91081834fd21
Raul Melo <50232299+Raulcarvalhomelo@users.noreply.github.com>
2026-08-06 02:16:23 +0000

    update error information provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,13 +3,13 @@ terraform {
       oci = {
         source = "oracle/oci"
         version = "~> 6.0"
-    
+        }
     }
 }
-provider "oci" 
-    tenancy_ocid = var.tenancy_ocid
-    user_ocid = var.user_ocid
-    fingerprint = var.fingerprint
-    private_key = var.private_key
-    region = var.region  
+provider "oci" {
+   tenancy_ocid = var.tenancy_ocid
+   user_ocid = var.user_ocid
+   region = var.region
+   fingerprint = var.fingerprint
+   private_key = var.private_key
 }
